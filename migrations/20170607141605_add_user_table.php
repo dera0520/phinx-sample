709a5fc6c62576ce3cb3f9b6ff592b3791888936
nobuhiro onodera <onodera@derabon.com>
2017-06-12 08:39:26 +0900

    add create seeds code

--- migrations/20170607141605_add_user_table.php
+++ migrations/20170607141605_add_user_table.php
@@ -13,6 +13,12 @@ class AddUserTable extends AbstractMigration
             CREATE TABLE `users` (
               `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
               `name` varchar(100) DEFAULT NULL,
+              `kana` varchar(100) DEFAULT NULL,
+              `login_code` varchar(30) DEFAULT NULL,
+              `password` varchar(30) DEFAULT NULL,
+              `birth_day` date DEFAULT NULL,
+              `email` varchar(255) DEFAULT NULL,
+              `created` datetime NOT NULL,
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
         ');
